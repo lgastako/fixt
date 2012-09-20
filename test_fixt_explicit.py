c160b1462121ac56d42d942efbbd6781c6f242a4
John Evans <lgastako@gmail.com>
2012-09-20 15:27:31 -0700

    Clean up tests a little, remove needless test abstraction wankery.

--- test_fixt_explicit.py
+++ test_fixt_explicit.py
@@ -8,22 +8,28 @@ from fixtcommontests import FixtCommonTests
 
 
 class TestExplicitInvocation(FixtCommonTests):
+    EX_PW_HASH = ('$2a$12$/lAOMdKlTaqZuiK5D8oPfuz'
+                  'l1WFwPFrxxXLYk9IAAfRs75dU1y/Y.')
 
-    def insert_to_users(self):
-        return (INSERT(users,
-                       username='lgastako',
-                       pw_hash=self.EX_PW_HASH))
+    def test_insert_to_users(self):
+        q = (INSERT(users,
+                    username='lgastako',
+                    pw_hash=self.EX_PW_HASH))
 
-    def select_from_users_with_aliases(self):
+        exq1 = ("INSERT INTO users (username, pw_hash)\n"
+                "VALUES (%(username)X, %(pw_hash)X)")
+        exq2 = ("INSERT INTO users (pw_hash, username)\n"
+                "VALUES (%(pw_hash)X, %(username)X)")
+        assert str(q) in (exq1, exq2)
+
+    def test_select_from_users_with_aliases(self):
         # Note we avoided having to explicitly import the pw_hash alias
         # by explicitly importing the u alias and then using that.  Obviously
         # this is more important when there are more attributes in the
         # projection.
 
-        y = u.pw_hash
-        assert str(y) == "u.pw_hash"
-        x = y.AS('bcrypt_pw')
-        assert str(x) == "u.pw_hash AS bcrypt_pw"
-        sel = SELECT(username, x)
-        fro = sel.FROM(users.AS('u'))
-        return fro
+        q = (SELECT(username, u.pw_hash.AS('bcrypt_pw'))
+                .FROM(users.AS('u')))
+
+        assert str(q) == ("SELECT username, u.pw_hash AS bcrypt_pw\n"
+                          "FROM users AS u")
--- test_fixt_implicit.py
+++ test_fixt_implicit.py
@@ -4,15 +4,25 @@ from fixtcommontests import FixtCommonTests
 
 
 class TestImplicitInvocation(FixtCommonTests):
+    EX_PW_HASH = ('$2a$12$/lAOMdKlTaqZuiK5D8oPfuz'
+                  'l1WFwPFrxxXLYk9IAAfRs75dU1y/Y.')
 
-    def insert_to_users(self):
-        return _.INSERT(_.users,
+    def test_insert_to_users(self):
+        q = _.INSERT(_.users,
                         username='lgastako',
                         pw_hash=self.EX_PW_HASH)
 
-    def select_from_users_with_aliases(self):
+        assert str(q) in (("INSERT INTO users (username, pw_hash)\n"
+                           "VALUES (%(username)X, %(pw_hash)X)"),
+                          ("INSERT INTO users (pw_hash, username)\n"
+                           "VALUES (%(pw_hash)X, %(username)X)"))
+
+    def test_select_from_users_with_aliases(self):
         # Note we could just use _.pw_hash here, but we use the u alias
-        # to preserve compatability with the explicit case.
-        sel = _.SELECT(_.username, _.u.pw_hash.AS('bcrypt_pw'))
-        fro = sel.FROM(_.users.AS('u'))
-        return fro
+        # to preserve parity with the explicit case.
+
+        q = (_.SELECT(_.username, _.u.pw_hash.AS('bcrypt_pw'))
+                .FROM(_.users.AS('u')))
+
+        assert str(q) == ("SELECT username, u.pw_hash AS bcrypt_pw\n"
+                          "FROM users AS u")
